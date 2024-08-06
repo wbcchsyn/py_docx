fde1e2467f5cc2b8bf14684a7cbc30fbea82a5e5
Yoshida Shin <wbcchsyn@gmail.com>
2024-08-06 11:50:04 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- python -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='docx',
+    version='0.0.1',
+    description='A simple python library to read docx files',
+    author='Shin Yoshida',
+    packages=find_packages(),
+    license='MIT',
+)
